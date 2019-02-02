cdd0068b82c9d44191ba22f60671e5417216d9c9
hakier <hakier29a@gmail.com>
2019-02-02 18:35:33 +0100

    Backend#34 Crawler: extract function extractFromDocument to public repo and publish to npm  * moved lib files to lib sub folder

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
-export * from './models';
-export * from './util';
+export * from './public_api';
---
+++ src/public_api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of extract-from-document
+ */
+
+export * from './lib/models';
+export * from './lib/util/extract-from-document';
