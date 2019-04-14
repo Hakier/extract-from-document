3edbeaf72d91d2c2805df7c5935f7952ce7d8424
hakier <hakier29a@gmail.com>
2019-04-14 09:06:34 +0200

    Backend#34 Crawler: extract function extractFromDocument to public repo and publish to npm  * optimized package size using .npmignore and adjusting public_api to export directly

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,4 +2,8 @@
  * Public API Surface of extract-from-document
  */
 
-export * from './lib';
+export * from './lib/models/map';
+export * from './lib/models/recipe';
+export * from './lib/models/scope';
+export * from './lib/models/source';
+export * from './lib/util/extract-from-document';
